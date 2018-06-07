2a84039b5fb95bf62514bf50dcca810dd96ba334
Nate <nathaniel.p.stewart@gmail.com>
2018-06-07 09:58:47 -0700

    Use bulk inserts to speed up dataset generation (#7)

--- movr.py
+++ movr.py
@@ -5,6 +5,7 @@ from faker import Faker
 from models import Base, User, Vehicle, Ride
 from generators import MovRGenerator
 import datetime
+import random
 
 #@todo: add query retries
 
@@ -26,7 +27,7 @@ class MovR:
     def start_ride(self, rider_id, vehicle_id):
         r = Ride(id = MovRGenerator.generate_uuid(),
                  rider_id=rider_id, vehicle_id=vehicle_id,
-                 start_address = MovR.fake.address())
+                 start_address = MovR.fake.address()) #@todo: this should be the address of the vehicle
         self.session.add(r)
         self.session.query(Vehicle).filter_by(id=vehicle_id).update({"status": "in_use"})
         self.session.commit()
@@ -34,7 +35,7 @@ class MovR:
 
     def end_ride(self, ride_id):
         ride = self.session.query(Ride).filter_by(id=ride_id).first()
-        ride.end_address = MovR.fake.address()
+        ride.end_address = MovR.fake.address() #@todo: this should update the address of the vehicle
         ride.revenue = MovRGenerator.generate_revenue()
         ride.end_time = datetime.datetime.now()
         self.session.query(Vehicle).filter_by(id = ride.vehicle_id).update({"status": "available"})
@@ -47,6 +48,48 @@ class MovR:
         self.session.commit()
         return u
 
+
+    def add_rides(self, num_rides):
+        chunk_size = 10000
+
+        users = self.session.query(User).all()
+        vehicles = self.session.query(Vehicle).all()
+
+        for chunk in range(0, num_rides, chunk_size):
+            rides = []
+            for i in range(chunk, min(chunk + chunk_size, num_rides)):
+                rides.append(Ride(id = MovRGenerator.generate_uuid(),
+                 rider_id=random.choice(users).id, vehicle_id=random.choice(vehicles).id,
+                 start_address = MovR.fake.address(),
+                 end_address = MovR.fake.address(),
+                 revenue = MovRGenerator.generate_revenue(),
+                 end_time=datetime.datetime.now()))
+            self.session.bulk_save_objects(rides)
+        self.session.commit()
+
+    def add_users(self, num_users):
+        chunk_size = 10000
+        for chunk in range(0, num_users, chunk_size):
+            users = []
+            for i in range(chunk, min(chunk + chunk_size, num_users)):
+                users.append(User(id = MovRGenerator.generate_uuid(), name = MovR.fake.name(),
+                      address = MovR.fake.address(), credit_card = MovR.fake.credit_card_number()))
+            self.session.bulk_save_objects(users)
+        self.session.commit()
+
+    def add_vehicles(self, num_vehicles, cities):
+        owners = self.session.query(User).all()
+        chunk_size = 10000
+        for chunk in range(0, num_vehicles, chunk_size):
+            vehicles = []
+            for i in range(chunk, min(chunk + chunk_size, num_vehicles)):
+                vehicle_type = MovRGenerator.generate_random_vehicle()
+                vehicles.append(Vehicle(id=MovRGenerator.generate_uuid(), type=vehicle_type,
+                          city=random.choice(cities), owner_id=(random.choice(owners)).id, status=MovRGenerator.get_vehicle_availability(),
+                          ext=MovRGenerator.generate_vehicle_metadata(vehicle_type)))
+            self.session.bulk_save_objects(vehicles)
+        self.session.commit()
+
     def get_users(self):
         return self.session.query(User).all()
 
@@ -58,10 +101,10 @@ class MovR:
 
     def add_vehicle(self, user_id, cities):
         vehicle_type = MovRGenerator.generate_random_vehicle()
-        ext = MovRGenerator.generate_vehicle_metadata(vehicle_type)
-        vehicle = Vehicle(id=MovRGenerator.generate_uuid(), type=vehicle_type, city=cities,
-                          owner_id=user_id,
-                          status=MovRGenerator.get_vehicle_availability(), ext=ext)
+
+        vehicle = Vehicle(id=MovRGenerator.generate_uuid(), type=vehicle_type,
+                          city=random.choice(cities), owner_id=user_id, status=MovRGenerator.get_vehicle_availability(),
+                          ext=MovRGenerator.generate_vehicle_metadata(vehicle_type))
 
         self.session.add(vehicle)
         self.session.commit()
